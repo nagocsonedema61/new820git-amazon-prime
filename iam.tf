266290ee15e8a6a9199d9e1d5590831166c3ee2d
moyo Edema <moyogbemiedema13@gmail.com>
2022-08-20 20:38:36 -0500

    new iam changes

---
+++ iam.tf
@@ -0,0 +1,9 @@
+ssddfeeolgotuuyprpwwooofihfhdddk
+djfhftitoslsalggg
+fffdldkgfjkghtkflsala;skrik
+fk;fffpriugkgjfdate "sdk;fjrj
+fkdkejeyshqoovmchgj
+ffhhfjfkfkfhfjdkdkdjfjfdsoeiy
+djfhfgdjdjkdfjfh" "name" {
+
+}
